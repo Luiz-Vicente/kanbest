68a171b0ed3357b0d0623340a5df1026172ba935
Luiz Fernando <fernandoluiz43673@gmail.com>
2022-10-13 00:59:27 -0300

    bootstrap v5 installation
    - installed bootstrap v5
    - start of color customization
    
    Signed-off-by: Luiz Vicente <fernandoluiz5217@gmail.com>

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <div>A fazer</div>
-    <div>Fazendo</div>
-    <div>Concluído</div>
+  <div class="bg-primary d-flex align-items-center justify-content-around">
+    <div class="d-flex flex-column">
+      <h2>A fazer</h2>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </div>
+    <div class="d-flex flex-column">
+      <h2>Fazendo</h2>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </div>
+    <div class="d-flex flex-column">
+      <h2>Concluído</h2>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </div>
   </div>
 </template>
